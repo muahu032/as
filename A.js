366470957ff360db76fbcd3eb15276783d5fc50f
muahu032 <MUAHU032@gmail.com>
2025-02-20 23:30:50 +0000

    Add files via upload

---
+++ A.js
@@ -0,0 +1,26 @@
+"code": 1,
+    "msg": "success",
+    "time": "1740093809",
+    "data": {
+        "id": 20155,
+        "username": "Minahj28",
+        "nickname": "Minahj28",
+        "email": "Minu23012004@gmail.com",
+        "mobile": "3441345403",
+        "avatar": "data:image\/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgaGVpZ2h0PSIxMDAiIHdpZHRoPSIxMDAiPjxyZWN0IGZpbGw9InJnYigxNjAsMjI5LDE2NikiIHg9IjAiIHk9IjAiIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj48L3JlY3Q+PHRleHQgeD0iNTAiIHk9IjUwIiBmb250LXNpemU9IjUwIiB0ZXh0LWNvcHk9ImZhc3QiIGZpbGw9IiNmZmZmZmYiIHRleHQtYW5jaG9yPSJtaWRkbGUiIHRleHQtcmlnaHRzPSJhZG1pbiIgZG9taW5hbnQtYmFzZWxpbmU9ImNlbnRyYWwiPk08L3RleHQ+PC9zdmc+",
+        "money": "0.00",
+        "vip_id": 1,
+        "invite_code": null,
+        "token": "ca234848-e581-49ff-93b8-044e3b1e5435",
+        "user_id": 20155,
+        "createtime": 1740093396,
+        "expiretime": 1742685396,
+        "expires_in": 2591586,
+        "vip": "VIP1",
+        "daily": 100,
+        "month": 1000,
+        "kf": "https:\/\/www.baidu.com",
+        "kf_email": "usdtff1@gmail.com"
+    }
+    
+}
